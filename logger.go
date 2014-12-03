a59fff699ece0391f0c72dd77790f3fb85f46016
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-12-02 21:42:20 -0500

    Initial commit

---
+++ logger.go
@@ -0,0 +1,106 @@
+package log
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type Level int
+
+const (
+	DEBUG Level = iota
+	INFO
+	NOTICE
+	WARN
+	ERROR
+	FATAL
+)
+
+var levels = map[Level]string{
+	DEBUG:  "DEBUG",
+	INFO:   "INFO",
+	NOTICE: "NOTICE",
+	WARN:   "WARN",
+	ERROR:  "ERROR",
+	FATAL:  "FATAL",
+}
+
+var (
+	LogLevel Level = INFO
+)
+
+func SetLogLevel(l Level) {
+	LogLevel = l
+}
+
+// Logger wraps go built-in log
+type Logger struct {
+	*log.Logger
+}
+
+// New returns a logger for one or more Writers
+func New(prefix string, flags int, w ...io.Writer) *Logger {
+	var wr io.Writer
+	if len(w) == 1 {
+		wr = w[0]
+	} else {
+		wr = io.MultiWriter(w...)
+	}
+
+	// no writers, output to Stderr
+	if wr == nil {
+		wr = os.Stderr
+	}
+
+	return &Logger{
+		Logger: log.New(wr, prefix, flags),
+	}
+}
+
+func (l Logger) Output(calldepth int, s string) error {
+	return l.Logger.Output(calldepth, s)
+}
+
+// Log provides log level checks before writing. As well as level string prefix
+// before the message
+func (l Logger) Log(lvl Level, p string, v ...interface{}) error {
+	s, ok := levels[lvl]
+	if !ok || lvl < LogLevel {
+		return nil
+	}
+	return l.Output(2, f(f("[%s] %s", s, p), v...))
+}
+
+// f is a short cut to fmt.Sprintf
+func f(p string, v ...interface{}) string {
+	return fmt.Sprintf(p, v...)
+}
+
+func (l Logger) Debug(p string, v ...interface{}) error {
+	return l.Log(DEBUG, p, v...)
+}
+
+func (l Logger) Info(p string, v ...interface{}) error {
+	return l.Log(INFO, p, v...)
+}
+
+func (l Logger) Notice(p string, v ...interface{}) error {
+	return l.Log(NOTICE, p, v...)
+}
+
+func (l Logger) Warn(p string, v ...interface{}) error {
+	return l.Log(WARN, p, v...)
+}
+
+func (l Logger) Error(p string, v ...interface{}) error {
+	return l.Log(ERROR, p, v...)
+}
+
+// Fatal exits process
+func (l Logger) Fatal(p string, v ...interface{}) error {
+	l.Log(FATAL, p, v...)
+	os.Exit(1)
+	return nil
+}
---
+++ logger_test.go
@@ -0,0 +1,55 @@
+package log
+
+import (
+	"bytes"
+	"github.com/nowk/assert"
+	"testing"
+)
+
+func TestMultipleWriters(t *testing.T) {
+	var _a []byte
+	var _b []byte
+	a := bytes.NewBuffer(_a)
+	b := bytes.NewBuffer(_b)
+
+	l := New("PREFIX: ", 0, a, b)
+	err := l.Output(2, "Hello World!")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "PREFIX: Hello World!\n", a.String())
+	assert.Equal(t, "PREFIX: Hello World!\n", b.String())
+}
+
+func TestWithLogLevelValue(t *testing.T) {
+	var b []byte
+	w := bytes.NewBuffer(b)
+
+	l := New("PREFIX: ", 0, w)
+	err := l.Log(INFO, "%s!", "Hello World")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "PREFIX: [INFO] Hello World!\n", w.String())
+}
+
+func TestOutputAtOrAbove(t *testing.T) {
+	SetLogLevel(WARN)
+	defer SetLogLevel(INFO)
+
+	var b []byte
+	w := bytes.NewBuffer(b)
+
+	l := New("", 0, w)
+	err := l.Info("%s!", "Hello World")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "", w.String())
+
+	err = l.Warn("All your bases!")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, "[WARN] All your bases!\n", w.String())
+}
