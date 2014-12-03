b269c48c7f6b043921a49e7698c9affb08159b33
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-12-03 17:10:28 -0500

    Reverse caseness of logger and Logger (interface is now the exposed)

--- logger.go
+++ logger.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-type logger interface {
+type Logger interface {
 	Output(calldepth int, s string) error
 }
 
@@ -39,13 +39,13 @@ func SetLogLevel(l Level) {
 	LogLevel = l
 }
 
-// Logger wraps go built-in log
-type Logger struct {
+// logger wraps go built-in log
+type logger struct {
 	*log.Logger
 }
 
 // New returns a logger for one or more Writers
-func New(prefix string, flags int, w ...io.Writer) *Logger {
+func New(prefix string, flags int, w ...io.Writer) *logger {
 	var wr io.Writer
 	if len(w) == 1 {
 		wr = w[0]
@@ -55,22 +55,23 @@ func New(prefix string, flags int, w ...io.Writer) *Logger {
 		wr = os.Stderr
 	}
 
-	return &Logger{
+	return &logger{
 		Logger: log.New(wr, prefix, flags),
 	}
 }
 
-func (l Logger) Output(calldepth int, s string) error {
+func (l logger) Output(calldepth int, s string) error {
 	return l.Logger.Output(calldepth, s)
 }
 
 // Log provides log level checks before writing. As well as level string prefix
 // before the message
-func Log(l logger, lvl Level, p string, v ...interface{}) error {
+func Log(l Logger, lvl Level, p string, v ...interface{}) error {
 	s, ok := levels[lvl]
 	if !ok || lvl < LogLevel {
 		return nil
 	}
+
 	return l.Output(2, f(f("[%s] %s", s, p), v...))
 }
 
@@ -79,32 +80,32 @@ func f(p string, v ...interface{}) string {
 	return fmt.Sprintf(p, v...)
 }
 
-func (l Logger) Log(lvl Level, p string, v ...interface{}) error {
+func (l logger) Log(lvl Level, p string, v ...interface{}) error {
 	return Log(l, lvl, p, v...)
 }
 
-func (l Logger) Debug(p string, v ...interface{}) error {
+func (l logger) Debug(p string, v ...interface{}) error {
 	return l.Log(DEBUG, p, v...)
 }
 
-func (l Logger) Info(p string, v ...interface{}) error {
+func (l logger) Info(p string, v ...interface{}) error {
 	return l.Log(INFO, p, v...)
 }
 
-func (l Logger) Notice(p string, v ...interface{}) error {
+func (l logger) Notice(p string, v ...interface{}) error {
 	return l.Log(NOTICE, p, v...)
 }
 
-func (l Logger) Warn(p string, v ...interface{}) error {
+func (l logger) Warn(p string, v ...interface{}) error {
 	return l.Log(WARN, p, v...)
 }
 
-func (l Logger) Error(p string, v ...interface{}) error {
+func (l logger) Error(p string, v ...interface{}) error {
 	return l.Log(ERROR, p, v...)
 }
 
 // Fatal exits process
-func (l Logger) Fatal(p string, v ...interface{}) error {
+func (l logger) Fatal(p string, v ...interface{}) error {
 	l.Log(FATAL, p, v...)
 	os.Exit(1)
 	return nil
