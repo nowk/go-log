a076d9dd94ad15a172e42ef1e807b9731dac615e
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2015-02-01 00:25:24 -0500

    Expose existing log flag consts

---
+++ constants.go
@@ -0,0 +1,15 @@
+package log
+
+import (
+	"log"
+)
+
+// consts from log
+const (
+	Ldate         = log.Ldate
+	Ltime         = log.Ltime
+	Lmicroseconds = log.Lmicroseconds
+	Llongfile     = log.Llongfile
+	Lshortfile    = log.Lshortfile
+	LstdFlags     = log.LstdFlags
+)
